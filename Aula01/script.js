ee4d6eeea1fc3624bdb08d9452216e61ca8b1409
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-16 22:27:33 -0300

    Aula 01

---
+++ Aula01/script.js
@@ -0,0 +1,23 @@
+//Desenvolver um script que permita somar um array de números consecutivos, de forma que se some o primeiro número com o segundo e o imprima através do console. 
+//Depois, temos que pegar este resultado e somar o terceiro número, e assim por diante, até termos terminado de percorrer todo o array.
+
+function somarElementos(arraySoma) {
+
+
+  let soma = 0;
+
+  arraySoma.forEach( i => {
+
+      soma = soma + i
+
+      console.log(soma)
+  });
+
+
+};
+
+
+const array = [2,4,6,8]
+
+
+somarElementos(array)
