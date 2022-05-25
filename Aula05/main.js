bfd097d349e3ad4c476ab68d8f26e96b173dc227
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-24 22:12:17 -0300

    deixando os nomes mais harmoniosos

--- Aula05/main.js
+++ Aula05/main.js
@@ -7,7 +7,7 @@ function trocarCor() {
 
     
     //bodyElement.classList.add('background-red')
-    bodyElement.classList.toggle('background-red')
+    bodyElement.classList.toggle('dark-mode')
 
 
     console.log('A cor foi trocada')
